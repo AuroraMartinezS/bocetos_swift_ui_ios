96105c7a00579567855bf611e7c1dab1978c60a7
alumno <alumno@pc-candelap.uacj.mx>
2025-03-31 10:52:14 -0600

    [AGREGA] estilo bonito a las pantallas, falta terminar

---
+++ RedesSociales/RedesSociales/RedesSociales/ui/menu_inicial/menu_aplicacion.swift
@@ -0,0 +1,30 @@
+//
+//  menu_aplicacion.swift
+//  RedesSociales
+//
+//  Created by alumno on 3/31/25.
+//
+
+import  SwiftUI
+
+struct MenuNavegacion: View {
+    @Environment(ControladorAplicacion.self) var controlador
+    
+    var body: some View {
+        TabView{
+            GeneralPublicaciones()
+                .tabItem { Label("Mensajes", systemImage: "envelope.fill") }
+                .badge(controlador.publicaciones.count)
+            
+            Text("Hola Mundis")
+                .tabItem { Label("Perfil", systemImage: "person.crop.circle.fill") }
+            Text("Hola desde pantalla3")
+                .tabItem { Label("Etiqueta de este label", systemImage: "circle") }
+        }
+    }
+}
+
+#Preview {
+    MenuNavegacion()
+        .environment(ControladorAplicacion())
+}
--- RedesSociales/RedesSociales/RedesSociales/ui/publicaciones/publicaciones_general.swift
+++ RedesSociales/RedesSociales/RedesSociales/ui/publicaciones/publicaciones_general.swift
@@ -10,11 +10,14 @@ import SwiftUI
 struct GeneralPublicaciones: View{
     
     //@StateObject var controlador = ControladorPublicaciones()
+    let esquinas_redondeadas = CGSize(width: 15, height: 15)
     @Environment(ControladorAplicacion.self) var controlador
     
     var body: some View {
         NavigationStack{
             ScrollView{
+                Text("Tus Mensajes")
+                    .font(.system(size: 40, weight: .heavy))
                 VStack{
                     ForEach(controlador.publicaciones){publicacion in
                         NavigationLink{
@@ -22,16 +25,36 @@ struct GeneralPublicaciones: View{
                         } label : {
                             
                             VStack{
-                                Text("\(publicacion.id)")
-                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                                Text("\(publicacion.title)")
-                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                                    .foregroundStyle(.indigo)
+                                HStack(){
+                                    Text("#\(publicacion.id)")
+                                        .fontWeight(.bold)
+                                        .foregroundStyle(.teal)
+                                        
+                                    Text("\(publicacion.title)")
+                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                        .foregroundStyle(Color("newSecondaryColor"))
+                                       
+                                        .frame(maxWidth: .infinity, alignment: .center)
+                                }
+                                .padding(6)
+                                .frame(maxWidth: .infinity)
+
+                                
                                 Text("\(publicacion.body)")
+                                    .foregroundStyle(.white)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .multilineTextAlignment(.leading)
                             }
                             .frame(maxWidth: .infinity)
-                            .padding(20)
-                            .background(.orange)
+                            .padding(30)
+                            .background(Color("newPrimaryColor"))
+                            .clipShape(RoundedRectangle(cornerSize: esquinas_redondeadas))
+                                    .shadow(color: .black, radius: 3, x: 0, y: 1)
+                                    .overlay(
+                                            RoundedRectangle(cornerRadius: 15)
+                                                .stroke(.teal, lineWidth: 2)
+                                        )
+
                             
                         }
                         .simultaneousGesture(TapGesture().onEnded({
